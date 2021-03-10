e5ca8114d9430b8af9fb0d39f0ad37f19a2dac78
Oliver Christensen <orc13a@live.dk>
2021-03-10 14:06:16 +0100

    updated ASCII sheet

--- sortWords.js
+++ sortWords.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 let rootPath = path.join(__dirname);
 let allWordsPath = rootPath + '/words/words.json';
+let ASCIIpath = rootPath + '/words/ASCII-sheet.json';
 
 let allWords;
+let ASCIIsheet;
 
 let wordList1 = fs.readFileSync("./words-list-1.txt", "utf-8");
 let wordsFromFile = wordList1.split('\n');
@@ -14,7 +16,13 @@ fs.readFile(allWordsPath, (err, data) => {
 
     allWords = JSON.parse(data);
 
-    sort();
+    fs.readFile(ASCIIpath, (err, data2) => {
+        if (err) throw err;
+    
+        ASCIIsheet = JSON.parse(data2);
+    
+        sort('a');
+    });
 });
 
 let start = true;
@@ -22,6 +30,8 @@ let firstChar = '';
 let secondChar = '';
 
 function sort(letter) {
+    var letterArr = allWords[letter.toUpperCase()];
+    
     for (let w = 0; w < wordsFromFile.length; w++) {
         var rawWord = wordsFromFile[w];
         var word = '';
@@ -31,13 +41,23 @@ function sort(letter) {
         } else {
             word = rawWord;
         }
-
-        
+        console.log(word);
+        calASCII(word);
     }
     
     let rawData = JSON.stringify(allWords, null, 2);
     
     fs.writeFile(allWordsPath, rawData, (err) => {
         if(err) throw err;
+    });
+}
+
+function calASCII(word) {
+    var wordSplitted = word.split('');
+    var letterASCIIsum = 0;
+
+    wordSplitted.forEach(letter => {
+        letterASCIIsum += ASCIIsheet[letter];
+        console.log(letterASCIIsum);
     });
 }
