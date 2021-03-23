b0e0fd0114b23319d698ee54582e0cc9c1600c1e
Oliver Christensen <orc13a@live.dk>
2021-03-23 22:12:54 +0100

    loadLetters | work in progress

---
+++ sorting/loadLetters.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require("fs");
+
+const rootPath = path.join(__dirname, '../');
+const lettersPath = rootPath + 'words/letter-arrays/';
+
+let alfa = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å"];
+
+let allLetters = {};
+
+const loadLetters = () => {
+    for (let l = 0; l < alfa.length; l++) {
+        let letter = alfa[l];
+
+        fs.readFile(`${lettersPath}${letter}.json`, (err, data) => {
+            if (err) throw err;
+
+            allLetters[letter] = JSON.parse(data)[letter];
+        });
+    }
+
+    console.log(allLetters);
+}
+
+loadLetters();
