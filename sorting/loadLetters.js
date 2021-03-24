96caf9f221443011003d34fdba7ff61a10b51e51
Oliver Christensen <orc13a@live.dk>
2021-03-24 10:30:47 +0100

    Update loadLetters.js

--- sorting/loadLetters.js
+++ sorting/loadLetters.js
@@ -6,22 +6,4 @@ const lettersPath = rootPath + 'words/letter-arrays/';
 
 let alfa = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å"];
 
-let allLetters = {};
 
-const loadLetters = () => {
-    for (let l = 0; l < alfa.length; l++) {
-        let letter = alfa[l];
-
-        fs.readFile(`${lettersPath}${letter}.json`, (err, data) => {
-            if (err) throw err;
-
-            allLetters[letter] = JSON.parse(data)[letter];
-
-            console.log(allLetters);
-        });
-    }
-
-    //console.log(allLetters);
-}
-
-loadLetters();
